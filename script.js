da4604ac46404b48374fc4db939e652406f6710e
PatrickStojek <patryk.stojek7@gmail.com>
2024-09-02 20:38:08 +0200

    complete the Counter constructor function

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-const Counter = function(selector) {
+const Counter = function (selector) {
     const container = document.querySelector(selector)
-    if(!container) throw new Error(`there is no container element`)
+
+    if (!container) throw new Error('Container element not found')
+
     this.selector = selector
     this.container = container
     this.number = 0
 }
 
-Counter.prototype.render = function() {
-     this.container.innerText = ''
-
-     const h1 = document.createElement('h1')
-     const button = document.createElement('button')
-     h1.innerText = this.number
-
-     this.container.appendChild(h1)
+Counter.prototype.init = function () {
+    this.render()
 }
-Counter.prototype.inc = function() {
+
+Counter.prototype.render = function () {
+
+    this.container.innerText = ''
+
+    const h1 = document.createElement('h1')
+    const button = document.createElement('button')
+
+    h1.innerText = this.number
+    button.innerText = '+'
+
+    const self = this
+
+    button.addEventListener(
+        'click',
+        function () {
+            self.inc()
+        }
+    )
+
+    this.container.appendChild(h1)
+    this.container.appendChild(button)
+
+}
+
+Counter.prototype.inc = function () {
     this.number = this.number + 1
+    this.render()
 }
 
-Counter.prototype.status = function() {
-    console.log('the current number is' + this.number)
+Counter.prototype.status = function () {
+    console.log('Current number is: ' + this.number)
+    return this.number
 }
-
 const counter1 = new Counter('.counter-1')
+counter1.init()
